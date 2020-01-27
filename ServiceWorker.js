853485a2c7c7ff5b5548dbb91ec484203547c732
Finnur Bragason <fbragason@outlook.com>
2020-01-27 16:22:53 +0800

    .

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,7 +5,7 @@ const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
-const staticCacheName = 'blazor-cache-v2';
+const staticCacheName = 'blazor-cache-v3';
 const requiredFiles = [
 "/_framework/blazor.boot.json",
 "/_framework/blazor.server.js",
